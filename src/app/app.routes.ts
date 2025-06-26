6d57b23cd376240631ce4c5ba7ed62c386b119a9
Павле Шмакић <sp190347d@student.etf.bg.ac.rs>
2025-06-26 21:51:39 +0200

    feat: add user and admin components with routing
    - Added User and Admin components with basic templates.
    - Updated app routing to include paths for User and Admin components.
    - Modified app component to include navigation links for routing.
    - Included necessary dependencies for Bootstrap and ng-bootstrap in package.json.
    - Added Angular localization support in main.ts and tsconfig.app.json.
    - Created basic stylesheets for User and Admin components.
    - Implemented unit tests for User and Admin components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { User } from './components/user/user';
+import { Admin } from './components/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'user',
+        component: User
+    },
+    {
+        path: 'admin',
+        component: Admin
+    },
+];
