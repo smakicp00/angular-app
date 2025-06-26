4ba8340f5e97a265f94e251dabea6d1c238d0029
Павле Шмакић <sp190347d@student.etf.bg.ac.rs>
2025-06-26 22:16:04 +0200

    feat: update user component with detailed user information and edit functionality: One way data binding: Interpolation, Property Binding,  Attribute Binding, Class Binding, Style Binding, Event Binding

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -8,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class User {
 
+  // One way data binding: Interpolation, Property Binding,
+  // Attribute Binding, Class Binding, Style Binding, Event Binding
+
+  userName: string = 'John Doe';
+  userRole: string = 'User';
+  userEmail: string = 'john.doe@example.com';
+  userBio: string = 'This is a sample user bio.';
+  tableClasses: string = 'table table-bordered table-striped table-hover';
+  tableStyles: { [key: string]: string } = {
+    'width': '80%',
+    'margin-top': '50px',
+    'margin-left': 'auto',
+    'margin-right': 'auto'
+  };
+  buttonClasses: string = 'btn btn-primary';
+  buttonStyles: { [key: string]: string } = {
+    'margin-top': '20px',
+    'margin-left': '10px'
+  };
+  // Two way data binding: ngModel
+  
+
+
+  constructor() {
+    // Initialization logic can go here
+    this.userName = 'John Doe';
+    this.userRole = 'User';
+    this.userEmail = 'john.doe@example.com';
+    this.userBio = 'This is a sample user bio.';
+
+  }
+
+  editUser() {
+    // Logic to edit user details
+    this.userName = 'Jane Doe';
+    this.userRole = 'Admin';
+    this.userEmail = 'jane.doe@example.com';
+    this.userBio = 'This is an updated user bio.';
+  }
 }
